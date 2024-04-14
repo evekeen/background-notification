1478beb5a64aa6d3d17bd49fb6d97a440fafed8b
Alexander Ivkin <a@ivkin.dev>
2024-04-14 11:23:16 -0400

    Initial Commit

---
+++ BackgroundNotification/BackgroundNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundNotificationApp.swift
+//  BackgroundNotification
+//
+//  Created by Alexander Ivkin on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
